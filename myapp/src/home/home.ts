7c96d0796b358ffec8f8b8a70e25dffb00a00124
nik089 <niteshy089@gmail.com>
2026-01-02 17:17:23 +0530

    fixed bug

---
+++ myapp/src/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  mobileOpen = false;
+
+  toggleMenu() {
+    this.mobileOpen = !this.mobileOpen;
+  }
+}
