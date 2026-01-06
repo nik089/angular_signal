4590d687e13a594571937334e6d7f79c76cdfa27
Nitesh <Nitesh.yadav@netoyed.com>
2026-01-06 14:58:00 +0530

    fixed routes

--- myapp/src/app/app.routes.ts
+++ myapp/src/app/app.routes.ts
@@ -6,10 +6,13 @@ export const routes: Routes = [
 
   {
     path: 'counter',
-    loadComponent: () =>
-      import('../counter/counter').then(m => m.Counter)
+    loadComponent: () => import('../counter/counter').then((m) => m.Counter),
+  },
+  {
+    path: 'ngrx',
+    loadComponent: () => import('../ngrx/ngrx/ngrx.component').then((m) => m.NgrxComponent),
   },
 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: 'home' },
 ];
--- myapp/src/home/home.ts
+++ myapp/src/home/home.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Header } from '../header/header';
+import { Component, computed, signal, Signal, WritableSignal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,5 +8,30 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.scss',
 })
 export class Home {
+  // writeable Signal;
+  data: WritableSignal<string | number | boolean> = signal<string | number | boolean>('nik');
 
+  // computed signal;
+  speed: Signal<number> = computed<number>(() => 90); // con;t update value
+
+  handleData() {
+    this.data.set('yadava'); // or
+    this.data.set(true); //
+    this.data.set(29);
+  }
+
+  //set vs update
+
+  data1: WritableSignal<string> = signal<string>('nik');
+
+  user: WritableSignal<string[]> = signal(['nik', 'piter', 'albert']);
+
+  setData() {
+    this.data1.set('yadav'); // becos "yadav" value not depend on privious value;
+  }
+
+  updateData() {
+    // if i want add "bruce" in array its depend on privious value the we used update method
+    this.user.update((item) => [...item, 'bruce']);
+  }
 }
