7eac8cb3b2bb778d6df07226c417025cb7aa3b2b
Nitesh <Nitesh.yadav@netoyed.com>
2026-01-06 14:57:39 +0530

    add set and updated signal

---
+++ myapp/src/ngrx/ngrx/ngrx.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ngrx',
+  imports: [],
+  standalone: true,
+  templateUrl: './ngrx.component.html',
+  styleUrl: './ngrx.component.scss',
+})
+export class NgrxComponent {}
