bbdceb7d290364e2388094a01bc1ed40141b623c
Nitesh <Nitesh.yadav@netoyed.com>
2026-01-06 15:37:05 +0530

    fixed bug

--- myapp/src/home/home.ts
+++ myapp/src/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal, Signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, signal, Signal, WritableSignal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -20,8 +20,7 @@ export class Home {
     this.data.set(29);
   }
 
-  //set vs update
-
+  //set vs update=====================================================================================================
   data1: WritableSignal<string> = signal<string>('nik');
 
   user: WritableSignal<string[]> = signal(['nik', 'piter', 'albert']);
@@ -33,5 +32,46 @@ export class Home {
   updateData() {
     // if i want add "bruce" in array its depend on privious value the we used update method
     this.user.update((item) => [...item, 'bruce']);
+  }
+
+  //====================================================================================================
+  // computed signals exmpale
+  x = 10;
+  y = 20;
+  z = this.x + this.y;
+
+  showValue() {
+    console.log(this.z); // 30
+    this.x = 100;
+    console.log(this.z); //30 (beacoz pahle hi adtion ho chuka hai)
+  }
+
+  //but i want if chnage any x ot y value then chnage also z value or i want updated z value
+
+  // used computed
+  x1 = signal(10);
+  y1 = signal(20);
+  z1 = computed(() => this.x1() + this.y1());
+
+  showValue1() {
+    console.log(this.z1()); // 30
+    this.x1.set(100);
+  }
+
+  // ==================================================================================================
+  // effect() ===> Effect ek fn() hai jo constructer ke under used hota jab signals upadte
+  // hota hai to esse hame pta clta hai kya update or chnages hua hai.
+
+  count = signal(0);
+  message = signal('Effect will show updates here...');
+  constructor() {
+    effect(() => {
+      this.message.set(`Count changed to: ${this.count()}`);
+      console.log('Effect ran:', this.count());
+    });
+  }
+
+  increment() {
+    this.count.update((c) => c + 1);
   }
 }
